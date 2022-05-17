e7d82d0738f801696d494cb32727e3e406165cfc
Daniel254 <Daniel.laskin@gmail.com>
2022-05-17 23:34:05 +0300

    Bug fix

--- src/index.js
+++ src/index.js
@@ -26,37 +26,38 @@ function renderSearchResult(data) {
   }
   if (data?.length > 1) {
     clearElement(refs.countryInfoEl);
-    refs.countryListEl.insertAdjacentHTML('afterbegin', createCountryList(data));
+    refs.countryListEl.innerHTML = createCountryList(data);
     return;
   }
   if (data?.length === 1) {
     clearElement(refs.countryListEl);
-    refs.countryInfoEl.insertAdjacentHTML('afterbegin', createCountryCard(data[0]));
+    refs.countryInfoEl.innerHTML = createCountryCard(data[0]);
     return;
   }
   clearElement(refs.countryInfoEl);
   clearElement(refs.countryListEl);
 }
-function createCountryCard(countryObj) {
-  const cardLayout = `<img src="${
-    countryObj.flags.svg
-  }" class="country-card__flag" alt="Flag" width="50"/>
-	<h1 class="country-card__h">${countryObj.name.official}</h1>
-	<p><span class="country-card__label">Capital:</span> ${countryObj.capital}</p>
-	<p><span class="country-card__label">Population:</span> ${countryObj.population}</p>
-	<p><span class="country-card__label">Languages:</span> ${Object.values(countryObj.languages).join(
-    ', ',
-  )}</p>`;
-  return cardLayout;
+function createCountryCard({
+  name: { official: name },
+  flags: { svg: flag },
+  capital,
+  population,
+  languages,
+}) {
+  return `<img src="${flag}" class="country-card__flag" alt="Flag" width="50"/>
+	<h1 class="country-card__h">${name}</h1>
+	<p><span class="country-card__label">Capital:</span> ${capital}</p>
+	<p><span class="country-card__label">Population:</span> ${population}</p>
+	<p><span class="country-card__label">Languages:</span> ${Object.values(languages).join(', ')}</p>`;
 }
 function createCountryList(countriesArr) {
   return countriesArr.reduce((acc, curr) => acc + createCountryListElement(curr), '');
 }
-function createCountryListElement({ flags, name }) {
+function createCountryListElement({ flags: { svg: flag }, name: { common: name } }) {
   return `<li class="country-list-item">
-	<img src="${flags.svg}" alt="${name.common} flag" class="country-list-item__flag" width="40"/>
+	<img src="${flag}" alt="${name} flag" class="country-list-item__flag" width="40"/>
 	<span class="country-list-item__country-name">
-	${name.common}
+	${name}
 	</span>
 	</li>`;
 }
